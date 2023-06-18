9acee4360247a108c96c65ccdf3dd7b564278eba
Eelmoli <feelmorelikes@gmail.com>
2023-06-18 16:51:36 +0700

    Добавление UI и unit тестов

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -12,9 +12,13 @@ protocol MoviesLoading {
 }
 
 struct MoviesLoader: MoviesLoading {
-    // MARK: - NetworkClient
-    private let networkClient = NetworkClient()
-
+  // MARK: - NetworkClient
+  private let networkClient: NetworkRouting
+  
+  init(networkClient: NetworkRouting = NetworkClient()) {
+      self.networkClient = networkClient
+  }
+    
     // MARK: - URL
     private var mostPopularMoviesUrl: URL {
         guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_0h5g7kfx") else {
@@ -22,7 +26,7 @@ struct MoviesLoader: MoviesLoading {
         }
         return url
     }
-
+    
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
         networkClient.fetch(url: mostPopularMoviesUrl) { result in
             switch result {
